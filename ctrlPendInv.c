a3cfdcc6084ab9d0fd00b75d52f9b38d525fbe86
Daniel Dominguez <dannickfox@gmail.com>
2018-06-11 03:13:47 -0500

    added comments

--- ctrlPendInv.c
+++ ctrlPendInv.c
@@ -11,7 +11,7 @@
 #include "gnuplotGen.c"
 
 
-
+// Pendulum paramters.
 double u = 0;
 double l = 1;
 double g = -9.81;
@@ -31,7 +31,7 @@ int main(void) {
     double x1 = M_PI / 4; // angle
     double x2 = 0; // angular velocity
 
-    plotMembership();
+    plotMembership(); // Plots membership ranges.
 
     // For graph generation.
     double **pendOut = malloc(N_RESULTS * sizeof(double*));
@@ -47,11 +47,14 @@ int main(void) {
         for (int j = 0; j < N_RESULTS; j++) pendOut[j][i] = *outputRes[j];
         x2 += intg_rk4(t, x1, delta, d_x2);
         x1 += intg_rk4(t, x2, delta, d_x1);
+        
+        // Calculation for acceleration by fuzzy controller.
         u = ctrl_u(x1, x2);
     }
-
+    // Generate output file.
     fileGen("pendulo.dat", N_RESULTS, N_steps + 1, pendOut, tagNames);
     
+    // Clear memory.
     for (int i = 0; i < N_RESULTS; i++) free(pendOut[i]);
     free(pendOut);
 
@@ -59,7 +62,8 @@ int main(void) {
 }
 
 double intg_rk4 (double x_i, double y_i, double h, double (*fcn)(double, double))
-{
+{   
+    // Runge-Kutta 4th order integration.
     double k1 = h * fcn(x_i, y_i);
     double k2 = h * fcn(x_i + (h / 2), y_i + (k1 / 2));
     double k3 = h * fcn(x_i + (h / 2), y_i + (k2 / 2));
@@ -69,13 +73,16 @@ double intg_rk4 (double x_i, double y_i, double h, double (*fcn)(double, double)
 }
 
 double d_x1 (double t, double x2) {
+    // function for change in angular position.
     return x2;
 }
 double d_x2 (double t, double x1) {
+    // function for change in angular velocity.
     return (u * cos(x1) + g * sin(x1)) / l;
 }
 
 void plotMembership (void) {
+    // generates file for membership functions.
     double limA = -2;
     double limB = 2;
     double step_sz = 0.02;
